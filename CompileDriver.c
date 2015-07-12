f6bd19416148fc51177e13260785f99cc5fdf3e5
Jesse <jesse.spencer@me.com>
2015-07-12 17:55:58 -0400

    Majority complete
    Issue with VM trace printing when the command arg for it is used

--- CompileDriver.c
+++ CompileDriver.c
@@ -1,16 +1,205 @@
 //
 //  
-//  Module 3
+//  
 //
 //
 //
 
 #include <stdio.h>
-#include "compileDriver.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+#define true 1
+#define false 0
+#define CALL_STRING_BUFFER 25
+
+
+// Global variables for command line arguments (compiler directives)
+// Implicitly initialized to 0 (because they are static variables)
+//
+int directivePrintLexemes;
+int directivePrintAssembly;
+int directivePrintVMTrace;
+
+
+// Functions
+void checkDirectives(int argc, const char* argv[]);
+void printLexemeList();
+void printAssemblyCode();
+void printVMExecutionTrace();
+
 
 int main(int argc, const char * argv[]) {
     
     
+    char callScanner[CALL_STRING_BUFFER];
+    char callParser[CALL_STRING_BUFFER];
+    char callPMachine[CALL_STRING_BUFFER];
+    
+    strcpy(callScanner, "./Scanner");
+    strcpy(callParser, "./Parser");
+    strcpy(callPMachine, "./PMachine");
+    
+    
+    // Check for the compiler directives (command line args)
+    checkDirectives(argc, argv);
+    
+    
+    if (directivePrintAssembly) {
+        strcat(callParser, " -a");
+    }
+    
+    
+    // Use to call each section and check for success, determine whether or not to continue
+    int returnValue = system(callScanner);
+    
+    if (returnValue < 0 || WEXITSTATUS(returnValue) != EXIT_SUCCESS) {
+        exit(EXIT_FAILURE);
+    }
+    
+    returnValue = system(callParser);
+    
+    if (returnValue < 0 || WEXITSTATUS(returnValue) != EXIT_SUCCESS) {
+        exit(EXIT_FAILURE);
+    }
+    
+    // After the Parser has successfully completed
+    printf("No errors, program is syntactically correct.\n");
+    
+    returnValue = system(callPMachine);
+    
+    
+    // Now check call respective funtions for the command line args
+    //
+    if (directivePrintLexemes) {
+        printLexemeList();
+    }
+    
+    if (directivePrintAssembly) {
+        printAssemblyCode();
+    }
+    
+    if (directivePrintVMTrace) {
+        printVMExecutionTrace();
+    }
+    
+    
+    
     
     return 0;
+}
+
+
+void checkDirectives(int argc, const char* argv[]) {
+    
+    
+    for (int i = 1; i < argc; i++) {
+        
+        if ( (strcmp(argv[i], "-l")) == 0) {
+            directivePrintLexemes = true;
+        }
+        
+        else if ( (strcmp(argv[i], "-a")) == 0) {
+            directivePrintAssembly = true;
+        }
+        
+        else if ( (strcmp(argv[i], "-v")) == 0) {
+            directivePrintVMTrace = true;
+        }
+        
+        else printf("Unrecognized directive: %s", argv[i]);
+        
+    }
+    
+    
+}
+
+
+void printLexemeList() {
+    
+    // Open lexeme list file
+    FILE* lexemeListPointer = fopen("lexemelist.txt", "r");
+    
+    
+    if ( ! lexemeListPointer) {
+        printf("lexemelist.txt could not be opened");
+        exit(-1);
+    }
+    
+    
+    int currentChar;
+    
+    while ( (currentChar = fgetc(lexemeListPointer)) != EOF) {
+        printf("%c", currentChar);
+    }
+    
+    printf("\n\n");
+    
+    
+    FILE* symbolTablePointer = fopen("symboltable.txt", "r");
+    
+    printf("\n\nPrinting out the symbol table:\n");
+    
+    if (symbolTablePointer == NULL) {
+        printf("\Error finding symbol table file output\n\n");
+        exit(1);
+    }
+    
+    while ( (currentChar = fgetc(symbolTablePointer)) != EOF ) {
+        printf("%c", currentChar);
+    }
+    
+    printf("\n\n");
+    
+    
+    fclose(lexemeListPointer);
+    fclose(symbolTablePointer);
+}
+
+void printAssemblyCode() {
+    
+    
+    FILE* parserPointer = fopen("mcode.txt", "r");
+    
+    
+    if (parserPointer == NULL) {
+        printf("\n\nError finding Parser output\n");
+        exit(1);
+    }
+    
+    int currentCharacter;
+    
+    while ( (currentCharacter = fgetc(parserPointer)) != EOF) {
+        printf("%c", currentCharacter);
+    }
+    
+    printf("\n\n");
+    
+    
+    fclose(parserPointer);
+}
+
+void printVMExecutionTrace() {
+    
+    FILE* VMPointer = fopen("stacktrace.txt", "r");
+    
+    printf("\n\nPrinting out Virtual Machine output:\n\n");
+    
+    // exit program if file not found
+    if (VMPointer == NULL) {
+        printf("\nError finding VM output\n");
+        exit(1);
+    }
+    
+    int currentChar;
+    
+    while ( (currentChar = fgetc(VMPointer)) != EOF) {
+        printf("%c", currentChar);
+    }
+    
+    printf("\n\n");
+    
+    
+    fclose(VMPointer);
 }
--- Pmachine.c
+++ Pmachine.c
@@ -16,416 +16,388 @@
 #include <stdio.h>
 
 
-// Constants
-//
-static const int MAX_STACK_HEIGHT = 2000;
-static const int MAX_CODE_LENGTH = 500;
-static const int MAX_LEXI_LEVELS = 3;
-//
-// Helpful constants
-#define TRUE 1
-#define FALSE 0
-#define MAX(x, y) (x>y?x:y)
-//
-// Enums to define codes
-//
-// op codes
-typedef enum {
-    
-    LIT = 1,
-    OPR = 2,
-    LOD = 3,
-    STO = 4,
-    CAL = 5,
-    INC = 6,
-    JMP = 7,
-    JPC = 8,
-    SIO = 9
-    
-} opcode;
-//
-// operators
-typedef enum {
-    
-    RET = 0,
-    NEG = 1,
-    ADD = 2,
-    SUB = 3,
-    MUL = 4,
-    DIV = 5,
-    ODD = 6,
-    MOD = 7,
-    EQL = 8,
-    NEQ = 9,
-    LSS = 10,
-    LEQ = 11,
-    GTR = 12,
-    GEQ = 13
-    
-} operator;
-//
-// List of strings for op codes
-static const char* opCodes[] = {
-    "",     // 00
-    "lit",  // 01
-    "opr",  // 02
-    "lod",  // 03
-    "sto",  // 04
-    "cal",  // 05
-    "inc",  // 06
-    "jmp",  // 07
-    "jpc",  // 08
-    "sio"   // 09
-};
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_STACK_HEIGHT 2000
+#define MAX_CODE_LENGTH 500
+#define MAX_LEXI_LEVELS 3
 
 
-// Data structs
-//
-// Instructions
-typedef struct {
-    // opcode, L, M
-    int op;
-    int l;
-    int m;
+typedef struct
+{
+    int op; // opcode
+    int r;  // reg
+    int l;  // L
+    int m;  // M
 } instruction;
 
+// function definitions
+void printCode( FILE *ofp, int code[][MAX_CODE_LENGTH], int codeLength );
 
-// Global storage
-//
-// PM/0 CPU resgiters
-int sp = 0; // stack pointer
-int bp = 1; // base pointer
-int pc = 0; // program counter
-instruction ir; // instruction register
-//
-// Array implementation of stack, defined using MAX_STACK_HEIGHT
-int stack[MAX_STACK_HEIGHT];
-//
-// Code memory store, defined using MAX_CODE_LENGTH
-instruction code[MAX_CODE_LENGTH];
+instruction fetch( int programCounter, int code[][MAX_CODE_LENGTH] );
+
+char* getOpCode( int opCode );
+
+int base( int l, int base, int *stack );
 
 
-// Function prototypes
-//
-int base(int level, int base);
-int loadFileToMemory(FILE* input);
-void outputInterpreted(FILE* output, int numberOfInstructions);
-void outputCurrent(FILE* output, int line, int activationRegisterList[]);
-void fetch();
-int execute(int activationRegisterList[], int* numberOfAR);
 
-
-int main(int argc, const char * argv[]) {
+// begin main
+int main(void)
+{
     
-    int isRunning = TRUE;
-    int numberOfAR = 0;
-    int currentLine;
+    int buffer, counter = 0, codeLen = 0;;
+    int i, j;
+    int line = 0;
     
-    int numberOfInstructions = 0;
+    // declare and initialize the stack
+    int stack[MAX_STACK_HEIGHT];
+    for ( i = 0; i < MAX_STACK_HEIGHT; i++ )
+    {
+        stack[i] = 0;
+    }
     
-    int activationRegisterList[MAX_STACK_HEIGHT];
     
-    // Initialize
-    //
-    // stack
-    stack[1] = 0;
-    stack[2] = 0;
-    stack[3] = 0;
-    //
-    // ir
+    // declare and reset the array that stores the program code
+    int code[4][MAX_CODE_LENGTH];
+    
+    for ( i = 0; i < MAX_CODE_LENGTH; i++ )
+    {
+        for ( j = 0; j < 4; j++ )
+        {
+            code[j][i] = 0;
+        }
+    }
+    
+    
+    // declare and initialize the CPU register variables
+    int stackPtr = 0;
+    int basePtr = 1;
+    int PC = 0;
+    instruction ir;
     ir.op = 0;
+    ir.r = 0;
     ir.l = 0;
     ir.m = 0;
     
+    int reg[16];
+    for ( i = 0; i < 16; i++ )
+    {
+        reg[i] = 0;
+    }
+    // keeps track of new activation records and their lengths
+    int ActRecLen[20];
+    int curActRec = 0;
     
-    // Open input file
-    FILE* input = fopen("mcode.txt", "r");
-    // Check for success
-    if (input == NULL) {
-        printf("Error accessing input file");
-        return -1;
+    int halt = 0;   // set halt flag to 0
+    
+    
+    FILE *ifp = fopen("mcode.txt", "rb");
+    
+    // exit program if file not found
+    if (ifp == NULL) {
+        printf("Code for virtual machine not found\n");
+        exit(1);
     }
     
-    // Load file to memory and record number of instructions
-    numberOfInstructions = loadFileToMemory(input);
-    // Check that return was successful
-    if (numberOfInstructions < 0) {
-        return -1;
-    }
-    
-    
-    // Create output file
-    FILE* output = fopen("stacktrace.txt", "w");
-    
-    // Print to output file the program in interpreted assmbly language with line numbers
-    outputInterpreted(output, numberOfInstructions);
-    
-    // Print initial state, called with line at -1 to print the format information
-    outputCurrent(output, -1, activationRegisterList);
-    
-    
-    // Running loop for execution
-    while(isRunning) {
+    // Read the file and put it into the code memory
+    while (fscanf(ifp, "%d", &buffer) != EOF)
+    {
+        code[counter % 4][counter/4] = buffer;
         
-        currentLine = pc;
+        counter++;
         
-        // Fetch
-        fetch();
+    }// end while loop
+    
+    // keep track of the # of lines of code
+    codeLen = counter / 4;
+    
+    fclose (ifp);
+    
+    /*      used for debugging:
+     // printout code to make sure reads in correctly.
+     for ( i = 0; i < codeLen; i++)
+     {
+     for ( j = 0; j < 4; j++ )
+     printf("%d ", code[j][i]);
+     printf("\n");
+     
+     }
+     */
+    
+    FILE *ofp = fopen("stacktrace.txt", "w+");
+    
+    // print the code to file
+    printCode(ofp, code, codeLen);
+    
+    
+    // print headers for the stack info
+    fprintf(ofp, "\t\t\t\t\tPC\tBP\tSP\tstack \n");
+    fprintf(ofp, "Initial values \t\t\t\t%d\t", PC);
+    fprintf(ofp, "%d\t", basePtr);
+    fprintf(ofp, "%d\t", stackPtr);
+    fprintf(ofp, "%d\n", stack[0]);
+    
+    
+    // fetch and execute cycle
+    while ( !halt )
+    {
+        line = PC;
+        // comment out next line - used for debugging:
+        //printf("line: %d\n", line);
         
-        // Execution
-        if ( ! execute(activationRegisterList, &numberOfAR) ) isRunning = FALSE;
-
-        // Check that stack does not overflow
-        if (sp > MAX_STACK_HEIGHT) {
-            printf("Stack size exceeded");
-            return -1;
+        // fetch the next line of code
+        ir = fetch( PC, code );
+        
+        // increment the program counter
+        PC = PC + 1;
+        
+        // execute cycle
+        switch ( ir.op )
+        {
+            case 1: // LIT
+                reg[ir.r] = ir.m;
+                break;
+            case 2: //  RTN
+                stackPtr = basePtr - 1;
+                basePtr = stack[stackPtr + 3];
+                PC = stack[stackPtr + 4];
+                curActRec--;
+                break;
+            case 3: //  LOD
+                reg[ir.r] = stack[ base(ir.l, basePtr, stack) + ir.m ];
+                break;
+            case 4: // STO
+                stack[ base(ir.l, basePtr, stack) + ir.m] = reg[ir.r];
+                break;
+            case 5: // CAL
+                stack[stackPtr + 1] = 0;
+                stack[stackPtr + 2] = base(ir.l, basePtr, stack);
+                stack[stackPtr + 3] = basePtr;
+                stack[stackPtr + 4] = PC;
+                basePtr = stackPtr + 1;
+                PC = ir.m;
+                break;
+            case 6: // INC
+                stackPtr = stackPtr + ir.m;
+                ActRecLen[curActRec+1]=ir.m + ActRecLen[curActRec];
+                curActRec++;
+                break;
+            case 7: // JMP
+                PC = ir.m;
+                break;
+            case 8: //  JPC
+                if ( reg[ir.r] == 0 ){
+                    PC = ir.m;
+                }
+                break;
+            case 9: //  SIO R, 0, 1
+                printf("%d\n", reg[ir.r] );
+                break;
+            case 10: // SIO R, 0, 2
+                printf("Enter value: \t");
+                scanf("%d", &reg[ir.r] );
+                break;
+            case 11:    // SIO R, 0, 3
+                halt = 1;
+                break;
+            case 12:    // NEG
+                reg[ ir.r ] = - reg[ir.l];
+                break;
+            case 13:    // ADD
+                reg[ir.r] = reg[ir.l] + reg[ir.m];
+                break;
+            case 14:    // SUB
+                reg[ir.r] = reg[ir.l] - reg[ir.m];
+                break;
+            case 15:    // MUL
+                reg[ir.r] = reg[ir.l] * reg[ir.m];
+                break;
+            case 16:    // DIV
+                reg[ir.r] = reg[ir.l] / reg[ir.m];
+                break;
+            case 17:    // ODD
+                reg[ir.r] = reg[ir.r] % 2;
+                break;
+            case 18:    // MOD
+                reg[ir.r] = reg[ir.l] % reg[ir.m];
+                break;
+            case 19:    // EQL
+                reg[ir.r] = reg[ir.l] == reg[ir.m];
+                break;
+            case 20:    // NEQ
+                reg[ir.r] = reg[ir.l] != reg[ir.m];
+                break;
+            case 21:    // LSS
+                reg[ir.r] = reg[ir.l] < reg[ir.m];
+                break;
+            case 22:    // LEQ
+                reg[ir.r] = reg[ir.l] <= reg[ir.m];
+                break;
+            case 23:    // GTR
+                reg[ir.r] = reg[ir.l] > reg[ir.m];
+                break;
+            case 24:    // GEQ
+                reg[ir.r] = reg[ir.l] >= reg[ir.m];
+                break;
+            default:
+                printf("Invalid op code.\n");
+                exit(1);
+                
         }
         
-        // Output current values to file
-        outputCurrent(output, currentLine, activationRegisterList);
         
-    }
+        // print registers
+        fprintf(ofp, "%d\t", line );
+        fprintf(ofp, "%s\t", getOpCode(ir.op) );
+        fprintf(ofp, "%d\t", ir.r);
+        
+        fprintf(ofp, "%d\t", ir.l);
+        fprintf(ofp, "%d\t", ir.m);
+        fprintf(ofp, "%d\t", PC);
+        fprintf(ofp, "%d\t", basePtr);
+        fprintf(ofp, "%d\t", stackPtr);
+        
+        // print the stack
+        i=1;
+        for( j = 0; j <= stackPtr; j++)
+        {
+            fprintf(ofp, "%d ", stack[j] );
+            // print out separators btwn the activation records
+            if ( j == ActRecLen[i] && j != stackPtr )
+            {
+                fprintf(ofp, "| ");
+                i++;
+            }
+        }
+        fprintf(ofp, "\n");
+        
+        
+        // set halt flag to 1 on the end condition
+        if ( stackPtr == 0 && basePtr == 0 && PC == 0)
+        {
+            halt = 1;
+        }
+        
+    }// end while loop
     
     
-    fclose(input);
-    fclose(output);
+    fclose(ofp);
+    
+    
     return 0;
-}
+    
+}// end function main
 
 
-int base(int level, int base) {
+// prints the code to the output file
+void printCode( FILE *ofp, int code[][MAX_CODE_LENGTH], int codeLength )
+{
+    int i, j;
     
-    while (level > 0) {
-        base = stack[base + 2];
-        level--;
-    }
+    fprintf(ofp, "line\tOP\tR\tL\tM\n");
     
-    return base;
-}
-
-int loadFileToMemory(FILE* input) {
-    
-    // Fill list code with file data
-    
-    int numberOfInstructions = 0;
-    
-    // Check file pointer and continue with loading
-    if (input) {
-        
-        // Loop to the end of the file
-        while ( ! feof(input)) {
-            
-            // Check that code does not exceed MAX_CODE_LENGTH and exit if so
-            if (numberOfInstructions >= MAX_CODE_LENGTH) {
-                printf("MAX_CODE_LENGTH exceeded. Halting...\n");
-                return -1;
-            }
-            
-            // Scan file
-            fscanf(input, "%d ", &code[numberOfInstructions].op);
-            fscanf(input, "%d ", &code[numberOfInstructions].l);
-            fscanf(input, "%d ", &code[numberOfInstructions].m);
-            
-            numberOfInstructions++;
+    for ( i = 0; i < codeLength; i++)
+    {
+        fprintf(ofp, "%d\t", i);
+        fprintf(ofp, "%s\t", getOpCode(code[0][i]) );
+        for ( j = 1; j < 4; j++)
+        {
+            fprintf(ofp, "%d\t", code[j][i]);
         }
-        
-        return numberOfInstructions;
-        
-    } else return -1;
-    
-    
-}
-
-void outputInterpreted(FILE* output, int numberOfInstructions) {
-    
-    int i = 0;
-    
-    // Output format info
-    fprintf(output, "\nLine   OP   L   M\n");
-    
-    // Print all the lines of interpreted input
-    for (i = 0; i < numberOfInstructions; i++) {
-        fprintf(output, "%4d   %s   %1d   %d\n", i, opCodes[code[i].op], code[i].l, code[i].m);
+        fprintf(ofp, "\n");
     }
     
-    fprintf(output, "\n");
-}
+    fprintf(ofp, "\n");
+}// end function printCode()
 
-void outputCurrent(FILE* output, int line, int activationRegisterList[]) {
+
+// get the new instruction record from the code
+instruction fetch( int programCounter, int code[][MAX_CODE_LENGTH] )
+{
+    instruction currentCode;
     
-    int i;
-    int k;
+    currentCode.op = code[0][programCounter];
+    currentCode.r = code[1][programCounter];
+    currentCode.l = code[2][programCounter];
+    currentCode.m = code[3][programCounter];
     
-    // Check if the info header has been created
-    if (line < 0) {
-        fprintf(output, "                 %3s %3s %3s  Stack\n", "pc", "bp", "sp");
+    return currentCode;
+}// end function fetch
+
+
+// function given as part of the assignment specs
+int base( int l, int base, int *stack )
+{
+    int b1;
+    b1 = base;
+    while ( l > 0 )
+    {
+        b1 = stack[b1 + 1];
+        l--;
     }
     
-    if (line < 0) {
-        fprintf(output, "Initial Values   ");
-    } else
-        fprintf(output, "%3d %s %3d %3d  ", line, opCodes[ir.op], ir.l, ir.m);
-    
-    
-    // Print registers
-    fprintf(output, "%3d %3d %3d  ", pc, bp, sp);
-    
-    // Print the current stack
-    for (i = 1, k = 0; i <= MAX(sp, (bp+2)); i++) {
-        
-        // Track when to insert a bar
-        if (activationRegisterList[k] == i) {
-            fprintf(output, "| ");
-            k++;
-        }
-        
-        fprintf(output, "%d ", stack[i]);
-    }
-    
-    fprintf(output, "\n");
-}
+    return b1;
+}// end function base
 
-// To fetch the instruction
-void fetch() {
-    
-    ir = code[pc++];
-    
-}
 
-// To execute the instruction
-int execute(int activationRegisterList[], int* numberOfAR) {
-    
-    
-    switch( (opcode) ir.op) {
-        
-        case LIT:
-            sp = sp + 1;
-            stack[sp] = ir.m;
-            break;
+// returns the opCode String associated with the given opCode int
+char* getOpCode( int opCode )
+{
+    switch ( opCode )
+    {
             
-        case OPR:
-            switch( (operator) ir.m) {
-                case RET:
-                    if(bp == 1) return FALSE;
-                    
-                    activationRegisterList[--(*numberOfAR)] = 0;
-                    sp = bp - 1;
-                    pc = stack[sp+3];
-                    bp = stack[sp+2];
-                    break;
-                    
-                case NEG:
-                    stack[sp] = -stack[sp];
-                    break;
-                    
-                case ADD:
-                    (sp)--;
-                    stack[sp] = stack[sp] + stack[sp+1];
-                    break;
-                    
-                case SUB:
-                    (sp)--;
-                    stack[sp] = stack[sp] - stack[sp+1];
-                    break;
-                    
-                case MUL:
-                    (sp)--;
-                    stack[sp] = stack[sp] * stack[sp+1];
-                    break;
-                    
-                case DIV:
-                    (sp)--;
-                    stack[sp] = stack[sp] / stack[sp+1];
-                    break;
-                    
-                case ODD:
-                    stack[sp] = stack[sp] % 2;
-                    break;
-                    
-                case MOD:
-                    (sp)--;
-                    stack[sp] = stack[sp] % stack[(sp)+1];
-                    break;
-                    
-                case EQL:
-                    (sp)--;
-                    stack[sp] = stack[sp] == stack[sp+1];
-                    break;
-                    
-                case NEQ:
-                    (sp)--;
-                    stack[sp] = stack[sp] != stack[sp+1];
-                    break;
-                    
-                case LSS:
-                    (sp)--;
-                    stack[sp] = stack[sp] < stack[sp+1];
-                    break;
-                    
-                case LEQ:
-                    (sp)--;
-                    stack[sp] = stack[sp] <= stack[sp+1];
-                    break;
-                    
-                case GTR:
-                    (sp)--;
-                    stack[sp] = stack[sp] > stack[sp+1];
-                    break;
-                    
-                case GEQ:
-                    (sp)--;
-                    stack[sp] = stack[sp] >= stack[sp+1];
-                    break;
-            }
-            break;
-            
-        case LOD:
-            stack[++sp] = stack[base(ir.l, bp) + ir.m];
-            break;
-            
-        case STO:
-            stack[base(ir.l, bp) + ir.m] = stack[sp];
-            sp = sp - 1;
-            break;
-            
-        case CAL:
-            activationRegisterList[(*numberOfAR)++] = sp + 1;
-            stack[sp + 1] = base(ir.l, bp);
-            stack[sp + 2] = bp;
-            stack[sp + 3] = pc;
-            bp = sp + 1;
-            pc = ir.m;
-            break;
-            
-        case INC:
-            sp = sp + ir.m;
-            break;
-            
-        case JMP:
-            pc = ir.m;
-            break;
-            
-        case JPC:
-            if ( ! stack[(sp)--])
-                pc = ir.m;
-            break;
-            
-        case SIO:
-            if (ir.m == 0) {
-                printf("%d", stack[sp]);
-                sp = sp - 1;
-            }
-            if (ir.m == 1) {
-                scanf("%d", &stack[++sp]);
-            }
-            // Halt execution
-            if (ir.m == 2) {
-                return FALSE;
-            }
-            break;
-    }
-    
-    
-    return TRUE;
-}
-
+        case 1:
+            return "LIT";
+        case 2:
+            return "RTN";
+        case 3:
+            return "LOD";
+        case 4:
+            return "STO";
+        case 5:
+            return "CAL";
+        case 6:
+            return "INC";
+        case 7:
+            return "JMP";
+        case 8:
+            return "JPC";
+        case 9:
+            return "SIO";
+        case 10:
+            return "SIO";
+        case 11:
+            return "SIO";
+        case 12:
+            return "NEG";
+        case 13:
+            return "ADD";
+        case 14:
+            return "SUB";
+        case 15:
+            return "MUL";
+        case 16:
+            return "DIV";
+        case 17:
+            return "ODD";
+        case 18:
+            return "MOD";
+        case 19:
+            return "EQL";
+        case 20:
+            return "NEQ";
+        case 21:
+            return "LSS";
+        case 22:
+            return "LEQ";
+        case 23:
+            return "GTR";
+        case 24:
+            return "GEQ";
+        default:
+            return "error";
+    }// end switch statement
+}// end function getOpCode
